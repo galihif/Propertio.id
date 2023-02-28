bb77a62900bc4c45e60a5046519d3bc512416f09
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-02-28 16:36:19 +0700

    make topbar comp

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/component/TopBar.kt
@@ -0,0 +1,29 @@
+package com.cinurawa.propertioid.ui.component
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun TopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = {
+                onMenuClick()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = ""
+                )
+            }
+        },
+        title = {
+            Text("Propertioid")
+        },
+    )
+}
