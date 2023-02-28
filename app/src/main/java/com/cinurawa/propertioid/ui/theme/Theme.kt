dea8ea689e1e58721be1f5b9e90e6826317eab54
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-02-28 16:36:09 +0700

    make same color palette for dark and light

--- app/src/main/java/com/cinurawa/propertioid/ui/theme/Theme.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/theme/Theme.kt
@@ -5,11 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+
+//    Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
 )
 
 private val LightColorPalette = lightColors(
