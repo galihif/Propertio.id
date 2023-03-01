6442cc867178cd2c6882792e369f0376be3176b1
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-01 13:32:01 +0700

    ui: disable dark theme

--- app/src/main/java/com/cinurawa/propertioid/ui/theme/Theme.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/theme/Theme.kt
@@ -2,50 +2,28 @@ package com.cinurawa.propertioid.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val LightColorPalette = lightColors(
     primary = Blue500,
     primaryVariant = Purple700,
     secondary = Teal200,
 
-//    Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-)
-
-private val LightColorPalette = lightColors(
-    primary = Blue500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
 fun PropertioidTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
 
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
