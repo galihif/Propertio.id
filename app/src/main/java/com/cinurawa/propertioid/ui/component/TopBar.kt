41475438c0eb23c441cde356d57322dbe1d017aa
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-01 09:36:32 +0700

    ui: add logo in topbar

--- app/src/main/java/com/cinurawa/propertioid/ui/component/TopBar.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/component/TopBar.kt
@@ -1,17 +1,25 @@
 package com.cinurawa.propertioid.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.cinurawa.propertioid.R
 
 @Composable
 fun TopBar(onMenuClick: () -> Unit) {
     TopAppBar(
+        modifier = Modifier,
         navigationIcon = {
             IconButton(onClick = {
                 onMenuClick()
@@ -23,7 +31,16 @@ fun TopBar(onMenuClick: () -> Unit) {
             }
         },
         title = {
-            Text("Propertioid")
+            Image(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 8.dp),
+                painter = painterResource(id = R.drawable.logo_topbar),
+                contentDescription = "",
+                alignment = Alignment.CenterStart
+            )
         },
+        backgroundColor = MaterialTheme.colors.background,
+        elevation = 0.dp
     )
 }
