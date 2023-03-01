02d8caf40f20e4645b0565c1220e390cc4b0983d
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-01 11:05:27 +0700

    ui: add hometab section

--- app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
@@ -4,21 +4,28 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.cinurawa.propertioid.ui.component.HomeBanner
+import com.cinurawa.propertioid.ui.component.HomeTab
 
 @Composable
 fun HomeScreen() {
+    var selectedMenu by remember { mutableStateOf(HomeTab.Beli) }
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth(),
         contentPadding = PaddingValues(24.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        item{
+        item {
             HomeBanner()
+        }
+        item {
+            HomeTab(selectedMenu) {
+                selectedMenu = it
+            }
         }
     }
 }
