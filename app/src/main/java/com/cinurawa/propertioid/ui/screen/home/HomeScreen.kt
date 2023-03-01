58dfc038b34a9892abd9cf7ddbfcff2627047cf4
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-01 10:11:48 +0700

    ui: column home and banner section

--- app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
@@ -1,9 +1,24 @@
 package com.cinurawa.propertioid.ui.screen.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cinurawa.propertioid.ui.component.HomeBanner
 
 @Composable
 fun HomeScreen() {
-    Text(text ="Home Screen")
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentPadding = PaddingValues(24.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        item{
+            HomeBanner()
+        }
+    }
 }
