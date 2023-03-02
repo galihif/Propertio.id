2a5b9f983279074f61948d414c8e3408dadaf29f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-02 16:55:52 +0700

    ui: state hoisting homesearch card

--- app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.cinurawa.propertioid.ui.screen.home
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,15 +9,42 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.cinurawa.propertioid.ui.component.HomeBanner
 import com.cinurawa.propertioid.ui.component.HomeSearchCard
 import com.cinurawa.propertioid.ui.component.HomeTab
+import com.cinurawa.propertioid.utils.Type
 
 @ExperimentalMaterialApi
 @Composable
 fun HomeScreen() {
+    val context = LocalContext.current
     var selectedMenu by remember { mutableStateOf(HomeTab.Beli) }
+
+    var selectedOption by remember { mutableStateOf("") }
+    val listOptions = listOf(
+        Type.Apartemen.value,
+        Type.Gudang.value,
+        Type.Kondominium.value,
+        Type.Kosan.value,
+        Type.Pabrik.value,
+        Type.Perkantoran.value,
+        Type.RuangUsaha.value,
+        Type.Ruko.value,
+        Type.Rumah.value,
+        Type.RumahKuno.value,
+        Type.Tanah.value,
+        Type.Villa.value
+    )
+
+    var keyword by remember { mutableStateOf("") }
+
+    LaunchedEffect(selectedOption) {
+        if (selectedOption != "") {
+            Toast.makeText(context, selectedOption, Toast.LENGTH_SHORT).show()
+        }
+    }
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth(),
@@ -31,7 +59,13 @@ fun HomeScreen() {
                 HomeTab(selectedMenu) {
                     selectedMenu = it
                 }
-                HomeSearchCard()
+                HomeSearchCard(
+                    options = listOptions,
+                    onOptionSelected = { selectedOption = it },
+                    selectedOption = selectedOption,
+                    keyword = keyword,
+                    onKeywordChanged = { keyword = it }
+                )
             }
         }
     }
