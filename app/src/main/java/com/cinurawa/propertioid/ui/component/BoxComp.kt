2908b07b167d70e6e55cb1d83eb4e890f8e0b63d
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-02 16:27:12 +0700

    refactor: move homesearch card to BoxComp.kt

--- app/src/main/java/com/cinurawa/propertioid/ui/component/BoxComp.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/component/BoxComp.kt
@@ -1,16 +1,21 @@
 package com.cinurawa.propertioid.ui.component
 
+import SelectDropdown
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.cinurawa.propertioid.R
@@ -38,5 +43,39 @@ fun HomeBanner() {
                 .matchParentSize()
                 .padding(16.dp)
         )
+    }
+}
+
+
+@ExperimentalMaterialApi
+@Composable
+fun HomeSearchCard() {
+    val context = LocalContext.current
+    val options = listOf("Option 1", "Option 2", "Option 3")
+    var selectedOption by remember { mutableStateOf("") }
+
+    LaunchedEffect(key1 = selectedOption) {
+        Toast.makeText(context, selectedOption, Toast.LENGTH_SHORT).show()
+    }
+    Card(
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
+        elevation = 8.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            SelectDropdown(
+                options = options,
+                selectedOption = selectedOption,
+                onOptionSelected = { newOption ->
+                    selectedOption = newOption
+                }
+            )
+        }
     }
 }
--- app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
@@ -1,17 +1,16 @@
 package com.cinurawa.propertioid.ui.screen.home
 
-import SelectDropdown
-import android.widget.Toast
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.cinurawa.propertioid.ui.component.HomeBanner
+import com.cinurawa.propertioid.ui.component.HomeSearchCard
 import com.cinurawa.propertioid.ui.component.HomeTab
 
 @ExperimentalMaterialApi
@@ -38,35 +37,3 @@ fun HomeScreen() {
     }
 }
 
-@ExperimentalMaterialApi
-@Composable
-fun HomeSearchCard() {
-    val context = LocalContext.current
-    val options = listOf("Option 1", "Option 2", "Option 3")
-    var selectedOption by remember { mutableStateOf("") }
-
-    LaunchedEffect(key1 = selectedOption){
-        Toast.makeText(context, selectedOption, Toast.LENGTH_SHORT).show()
-    }
-    Card(
-        modifier = Modifier
-            .fillMaxWidth(),
-        shape = RoundedCornerShape(8.dp),
-        elevation = 8.dp
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            SelectDropdown(
-                options = options,
-                selectedOption = selectedOption,
-                onOptionSelected = { newOption ->
-                    selectedOption = newOption
-                }
-            )
-        }
-    }
-}
