9981971184bd2e6b1e71d6bc58f17c9a2e9d8088
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-02 19:43:29 +0700

    ui: add rounded corner

--- app/src/main/java/com/cinurawa/propertioid/ui/component/input/TextBox.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/component/input/TextBox.kt
@@ -1,8 +1,6 @@
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -33,6 +31,7 @@ fun SelectDropdown(
                     expanded = expanded
                 )
             },
+            shape = RoundedCornerShape(6.dp),
             modifier = Modifier.fillMaxWidth()
         )
         // menu
