89af470bafd239b6fff29af1a562a0a48863ea86
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-02 16:54:50 +0700

    feat: create enum class to store type value

---
+++ app/src/main/java/com/cinurawa/propertioid/utils/Type.kt
@@ -0,0 +1,16 @@
+package com.cinurawa.propertioid.utils
+
+enum class Type(val value: String) {
+    Apartemen("Apartemen"),
+    Gudang("Gudang"),
+    Kondominium("Kondominium"),
+    Kosan("Kosan"),
+    Pabrik("Pabrik"),
+    Perkantoran("Perkantoran"),
+    RuangUsaha("Ruang Usaha"),
+    Ruko("Ruko"),
+    Rumah("Rumah"),
+    RumahKuno("Rumah Kuno"),
+    Tanah("Tanah"),
+    Villa("Villa"),
+}
