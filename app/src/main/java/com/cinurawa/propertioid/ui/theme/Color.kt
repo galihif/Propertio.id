35c2e0cf571a5aa06ce8b4f60dd5a295cf98a2c8
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-05 12:52:24 +0700

    feat: add red and purple color

--- app/src/main/java/com/cinurawa/propertioid/ui/theme/Color.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.cinurawa.propertioid.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
 val Blue500 = Color(0xFF0BC0EB)
+val Red500 = Color(0xFFCB3A31)
+val Purple500 = Color(0xFF4C4DDC)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
