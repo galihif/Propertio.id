60d0448b15831539a2dff7909f8c646a4d38d107
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-07 16:28:53 +0700

    ui: create rounded image

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/atoms/Image.kt
@@ -0,0 +1,30 @@
+package com.cinurawa.propertioid.ui.atoms
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RoundedImage(
+    modifier: Modifier = Modifier,
+    @DrawableRes image: Int,
+    cornerRadius: Int = 14
+) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = "",
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .height(200.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(cornerRadius.dp))
+    )
+}
