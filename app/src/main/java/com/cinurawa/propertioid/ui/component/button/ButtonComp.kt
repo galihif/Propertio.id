434d87dc6704e396ce97cc6adda2bdfb9da786b0
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-07 11:04:54 +0700

    ui: create button lihat semua comp

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/component/button/ButtonComp.kt
@@ -0,0 +1,31 @@
+package com.cinurawa.propertioid.ui.component.button
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ButtonLihatSemua(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(5.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.primary.copy(0.1f),
+            contentColor = MaterialTheme.colors.primary
+        ),
+        elevation = ButtonDefaults.elevation(0.dp),
+        contentPadding = PaddingValues(vertical = 14.dp)
+    ) {
+        Text(text = "Lihat Semua")
+    }
+}
