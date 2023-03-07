d57bce523b843e12ec8e19ba2a7219fa870b827c
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-07 11:37:13 +0700

    ui: make section title font size to 24

--- app/src/main/java/com/cinurawa/propertioid/ui/component/text/TextComp.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/component/text/TextComp.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeSectionTitle(
@@ -15,6 +16,7 @@ fun HomeSectionTitle(
         text = title,
         style = MaterialTheme.typography.h6,
         modifier = modifier,
-        textAlign = TextAlign.Center
+        textAlign = TextAlign.Center,
+        fontSize = 24.sp
     )
 }
