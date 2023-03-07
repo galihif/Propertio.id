b047dc8b7338613fdc2fb0cd3de83f0c7a3b0e59
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-07 10:57:23 +0700

    ui: add lihat semua button

--- app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
@@ -3,9 +3,8 @@ package com.cinurawa.propertioid.ui.screen.home
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -84,6 +83,21 @@ fun HomeScreen() {
         items(10) {
             ItemProperty()
             Spacer(modifier = Modifier.height(24.dp))
+        }
+        item {
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(5.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = MaterialTheme.colors.primary.copy(0.1f),
+                    contentColor = MaterialTheme.colors.primary
+                ),
+                elevation = ButtonDefaults.elevation(0.dp),
+                contentPadding = PaddingValues(vertical = 14.dp)
+            ) {
+                Text(text = "Lihat Semua")
+            }
         }
     }
 }
