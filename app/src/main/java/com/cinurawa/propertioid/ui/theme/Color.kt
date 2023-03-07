279c19c9b9c220b2f38e2e0d9811c2f50bdda78d
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-07 12:47:37 +0700

    ui: add blue700 and darkblue500 color

--- app/src/main/java/com/cinurawa/propertioid/ui/theme/Color.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/theme/Color.kt
@@ -3,7 +3,13 @@ package com.cinurawa.propertioid.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Blue500 = Color(0xFF0BC0EB)
+val Blue700 = Color(0xFF23A4DE)
+
+val DarkBlue500 = Color(0xFF0061A8)
+
 val Red500 = Color(0xFFCB3A31)
+
 val Purple500 = Color(0xFF4C4DDC)
 val Purple700 = Color(0xFF3700B3)
+
 val Teal200 = Color(0xFF03DAC5)
