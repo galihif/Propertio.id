7fca47090994db7d39c1297fd7a6f13737c4d868
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-07 16:12:29 +0700

    refactor: create primarybutton atoms

--- app/src/main/java/com/cinurawa/propertioid/ui/atoms/Button.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/atoms/Button.kt
@@ -1,13 +1,16 @@
 package com.cinurawa.propertioid.ui.atoms
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -27,5 +30,34 @@ fun LihatSemuaButton(
         contentPadding = PaddingValues(vertical = 14.dp)
     ) {
         Text(text = "Lihat Semua")
+    }
+}
+
+@Composable
+fun PrimaryButton(
+    modifier: Modifier = Modifier,
+    title: String,
+    icon: ImageVector? = null,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = modifier.fillMaxWidth(),
+        onClick = onClick,
+        shape = RoundedCornerShape(6.dp),
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = title,
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center
+            )
+            if (icon != null) {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = "",
+                    modifier = Modifier.padding(start = 8.dp)
+                )
+            }
+        }
     }
 }
