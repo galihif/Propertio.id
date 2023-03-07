56ca898a1eeb70bdd08678c68fcd6d4431eb0ec9
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-07 13:00:59 +0700

    ui: add mari kami pandu card

--- app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/screen/home/HomeScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -148,7 +150,7 @@ fun HomeScreen() {
                     }
                 }
             }
-            Spacer(modifier =Modifier.height(14.dp))
+            Spacer(modifier = Modifier.height(14.dp))
             Row(horizontalArrangement = Arrangement.spacedBy(14.dp)) {
                 Card(
                     modifier = Modifier
@@ -215,6 +217,50 @@ fun HomeScreen() {
                     }
                 }
 
+            }
+        }
+        item {
+            Card(
+                Modifier.wrapContentSize(),
+                shape = RoundedCornerShape(10.dp),
+                backgroundColor = MaterialTheme.colors.primary,
+            ) {
+                Column(
+                    Modifier.fillMaxWidth()
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(150.dp)
+                            .clip(RoundedCornerShape(10.dp, 10.dp, 0.dp, 0.dp)),
+                        painter = painterResource(id = R.drawable.home_banner),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop
+                    )
+                    Column(
+                        Modifier.padding(14.dp)
+                    ) {
+                        Text(
+                            "Mari kami pandu ke rumah Impianmu",
+                            color = MaterialTheme.colors.onPrimary,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Button(
+                            onClick = { /*TODO*/ },
+                            shape = RoundedCornerShape(6.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = MaterialTheme.colors.surface,
+                                contentColor = MaterialTheme.colors.primary
+                            ),
+                            contentPadding = PaddingValues(10.dp),
+                            elevation = ButtonDefaults.elevation(0.dp)
+                        ) {
+                            Text("Selengkapnya")
+                        }
+                    }
+                }
             }
         }
     }
