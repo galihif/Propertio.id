0359a5b85213c4b84b1fb12a580dbd0ded0ba33d
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-09 13:38:53 +0700

    utils: add DataProvider.kt to provide list type

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/utils/DataProvider.kt
@@ -0,0 +1,20 @@
+package com.cinurawa.propertioid.ui.utils
+
+object DataProvider {
+    fun typeList(): List<String> {
+        return listOf(
+            "Apartemen",
+            "Gudang",
+            "Kondominium",
+            "Kosan",
+            "Pabrik",
+            "Perkantoran",
+            "Ruang Usaha",
+            "Ruko",
+            "Rumah",
+            "Rumah Kuno",
+            "Tanah",
+            "Villa"
+        )
+    }
+}
