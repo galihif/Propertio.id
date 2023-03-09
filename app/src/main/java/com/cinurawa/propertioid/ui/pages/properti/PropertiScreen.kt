505051d16ba76dcaee8b4d4c7f036fe9eb19fa00
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-09 13:47:28 +0700

    ui: add list iklan properti section

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/properti/PropertiScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/properti/PropertiScreen.kt
@@ -1,13 +1,14 @@
 package com.cinurawa.propertioid.ui.pages.properti
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.cinurawa.propertioid.ui.atoms.TitleSectionText
+import com.cinurawa.propertioid.ui.organisms.PropertyItem
 import com.cinurawa.propertioid.ui.organisms.PropertySearchBox
 import com.cinurawa.propertioid.ui.theme.Green500
 import com.cinurawa.propertioid.ui.utils.DataProvider
@@ -18,7 +19,9 @@ fun PropertiScreen() {
     var selectedOption by remember { mutableStateOf("") }
     val listOptions = DataProvider.typeList()
     var keyword by remember { mutableStateOf("") }
-    LazyColumn{
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ){
         item {
             Card(
                 modifier = Modifier
@@ -34,6 +37,22 @@ fun PropertiScreen() {
                     onKeywordChanged = { keyword = it }
                 )
             }
+        }
+        item {
+            TitleSectionText(
+                title = "List Iklan Properti",
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        items(5) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+            ){
+                PropertyItem()
+            }
+            Spacer(modifier = Modifier.height(24.dp))
         }
     }
 }
