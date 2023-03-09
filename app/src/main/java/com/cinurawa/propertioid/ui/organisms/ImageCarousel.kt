e1c7954cf26b4c6dfb58500b4b4d8ac93ce34de5
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-09 14:50:44 +0700

    ui: add modifier

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/organisms/ImageCarousel.kt
@@ -0,0 +1,41 @@
+package com.cinurawa.propertioid.ui.organisms
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cinurawa.propertioid.R
+import com.cinurawa.propertioid.ui.atoms.MyImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
+
+@ExperimentalPagerApi
+@Composable
+fun ImageCarousel(
+    modifier: Modifier = Modifier
+) {
+    val pagerState = rememberPagerState()
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HorizontalPager(
+            count = 8, state = pagerState,
+        ) { index ->
+            MyImage(image = R.drawable.home_banner)
+        }
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp),
+            activeColor = MaterialTheme.colors.primary,
+            inactiveColor = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        )
+    }
+}
