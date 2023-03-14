e458dd6f996ec7e346b066a08b734bc600b8b433
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-14 10:31:53 +0700

    ui: create HargaColumn.kt molecule

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/molecules/HargaColumn.kt
@@ -0,0 +1,27 @@
+package com.cinurawa.propertioid.ui.molecules
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HargaColumn(
+    harga:Int
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        Text(
+            text = "Harga",
+            style = MaterialTheme.typography.body2
+        )
+        Text(
+            text = "Rp. $harga",
+            style = MaterialTheme.typography.h6,
+            color = MaterialTheme.colors.primary
+        )
+    }
+}
