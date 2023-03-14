bec4b172591f1f78b3e8cbaafe307f81e7ad3ecb
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-14 11:15:53 +0700

    ui: create propertyattribute text atom

--- app/src/main/java/com/cinurawa/propertioid/ui/atoms/Text.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/atoms/Text.kt
@@ -4,7 +4,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -18,5 +22,28 @@ fun TitleSectionText(
         modifier = modifier,
         textAlign = TextAlign.Center,
         fontSize = 24.sp
+    )
+}
+
+@Composable
+fun PropertyAttributeText(
+    attribute:String,
+    value:String
+) {
+    Text(
+        buildAnnotatedString {
+            withStyle(
+                style = MaterialTheme.typography.subtitle1.toSpanStyle()
+                    .copy(color = Color.Gray)
+            ) {
+                append("$attribute : ")
+            }
+            withStyle(
+                style = MaterialTheme.typography.subtitle1.toSpanStyle()
+                    .copy(fontWeight = FontWeight.Bold)
+            ) {
+                append(value)
+            }
+        }
     )
 }
