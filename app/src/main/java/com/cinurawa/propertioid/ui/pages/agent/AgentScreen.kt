da1e4fa8d5876845a2c103f550a227ba3e655c78
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-24 21:53:29 +0700

    ui: create AgentScreen.kt

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/agent/AgentScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/agent/AgentScreen.kt
@@ -1,9 +1,37 @@
 package com.cinurawa.propertioid.ui.pages.agent
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.cinurawa.propertioid.ui.atoms.TitleSectionText
+import com.cinurawa.propertioid.ui.organisms.AgentItem
 
 @Composable
 fun AgentScreen() {
-    Text(text ="Agent Screen")
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ){
+        item {
+            TitleSectionText(
+                title = "List Agent",
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        items(5) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+            ) {
+                AgentItem(
+                    onDetailClicked = {
+
+                    }
+                )
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
 }
