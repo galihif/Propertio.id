e70e7eaad0f4dd9b7fe10cf1d0eb4c1e7eb1dcbc
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-27 21:23:18 +0700

    theme: add default font to inter

--- app/src/main/java/com/cinurawa/propertioid/ui/theme/Type.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/theme/Type.kt
@@ -2,14 +2,22 @@ package com.cinurawa.propertioid.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cinurawa.propertioid.R
 
 // Set of Material typography styles to start with
+val fonts = FontFamily(
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_bold, FontWeight.Bold),
+)
 val Typography = Typography(
+    defaultFontFamily = fonts,
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
