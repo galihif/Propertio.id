bf3e4b11ba70badf603ed090cac2cf8b332e9e6a
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-28 09:07:52 +0700

    theme: change h6 fontsize and fontweight

--- app/src/main/java/com/cinurawa/propertioid/ui/theme/Type.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/theme/Type.kt
@@ -23,7 +23,11 @@ val Typography = Typography(
     body1 = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
+    ),
+    h6 = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
