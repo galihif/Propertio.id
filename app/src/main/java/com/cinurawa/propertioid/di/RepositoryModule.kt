484df82d5db8513849d916013030386ba69cc97a
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-30 21:53:45 +0700

    di: create RepositoryModule.kt

---
+++ app/src/main/java/com/cinurawa/propertioid/di/RepositoryModule.kt
@@ -0,0 +1,41 @@
+package com.cinurawa.propertioid.di
+
+import com.cinurawa.propertioid.data.MainRepository
+import com.cinurawa.propertioid.data.remote.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dev.propertio.id/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMainRepository(apiService: ApiService) = MainRepository(apiService)
+
+}
