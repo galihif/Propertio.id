97c7e0fb39d9b47a06fdf7b117a3460395265979
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-01 20:31:54 +0700

    data: get all property from api

--- app/src/main/java/com/cinurawa/propertioid/data/MainRepository.kt
+++ app/src/main/java/com/cinurawa/propertioid/data/MainRepository.kt
@@ -1,10 +1,23 @@
 package com.cinurawa.propertioid.data
 
 import com.cinurawa.propertioid.data.remote.api.ApiService
+import com.cinurawa.propertioid.data.remote.dto.GetAllPropertyDto
+import com.cinurawa.propertioid.utils.Resource
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
     private val apiService: ApiService
 ) {
-    val tes = "tes"
+
+    fun getAllProperty() =
+        flow<Resource<List<GetAllPropertyDto.PropertyData>>>{
+            emit(Resource.Loading())
+            try {
+                val response = apiService.getAllProperty()
+                emit(Resource.Success(response.data))
+            } catch (e: Exception) {
+                emit(Resource.Error(e.message ?: "Error"))
+            }
+        }
 }
