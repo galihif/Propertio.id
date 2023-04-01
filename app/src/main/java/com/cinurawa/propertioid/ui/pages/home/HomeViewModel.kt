8cf5442b38c45698a79ebaf7bbb307b2cbd16549
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-01 20:31:15 +0700

    ui: get list property from repo

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeViewModel.kt
@@ -1,10 +1,13 @@
 package com.cinurawa.propertioid.ui.pages.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cinurawa.propertioid.data.MainRepository
+import com.cinurawa.propertioid.data.remote.dto.GetAllPropertyDto
+import com.cinurawa.propertioid.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,13 +15,38 @@ class HomeViewModel
 @Inject constructor(
     private val repo: MainRepository
 ) : ViewModel() {
-    val _tes = MutableStateFlow("")
 
-    fun getTest() {
-        _tes.value = repo.tes
+    var _listProperty = MutableStateFlow<List<GetAllPropertyDto.PropertyData>>(emptyList())
+    val listProperty = _listProperty
+
+    var _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading
+
+    var _error = MutableStateFlow("")
+    val error = _error
+
+    private fun getListProperty(){
+        viewModelScope.launch {
+            repo.getAllProperty().collect {
+                when(it){
+                    is Resource.Loading -> {
+                        _isLoading.value = true
+                    }
+                    is Resource.Success -> {
+                        _isLoading.value = false
+                        _listProperty.value = if (it.data.isNullOrEmpty()) emptyList() else it.data
+                    }
+                    is Resource.Error -> {
+                        _isLoading.value = false
+                        _error.value = it.message ?: "Error"
+                    }
+                }
+            }
+        }
     }
 
+
     init {
-        getTest()
+        getListProperty()
     }
 }
