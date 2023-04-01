4d913c310bbcd739d9455935b9876a5f23f432e8
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-01 20:29:11 +0700

    data: create get all property method

--- app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
+++ app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
@@ -1,4 +1,11 @@
 package com.cinurawa.propertioid.data.remote.api
 
+import com.cinurawa.propertioid.data.remote.dto.GetAllPropertyDto
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("mobile/get_all_property_active")
+    suspend fun getAllProperty(
+
+    ):GetAllPropertyDto
 }
