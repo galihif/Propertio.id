9fd78f11b2bb8df0991551fad83131411e73577f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-03 13:17:56 +0700

    data: create model Property.kt

---
+++ app/src/main/java/com/cinurawa/propertioid/data/model/Property.kt
@@ -0,0 +1,48 @@
+package com.cinurawa.propertioid.data.model
+data class Property(
+    val id: Int,
+    val name: String,
+    val desc: String,
+    val address: String,
+    val price: Int,
+    val propertyCode: String,
+
+    val photo: List<String>,
+
+    val type: String,
+    val listingType: String,
+    val certificate: String,
+
+    val condition: String,
+    val facing: String,
+    val yearBuilt: Int,
+
+    val floor: Int,
+    val surfaceArea: Int,
+    val buildingArea: Int,
+    val bedroom: Int,
+    val bathroom: Int,
+    val garage: Int,
+    val carport: Int,
+    val maidBedroom: Int,
+    val maidBathroom: Int,
+    val powerSupply: String,
+    val waterType: String,
+    val phoneLine: Int,
+    val isFurniture: Boolean,
+
+    val virtualTour: String? = null,
+    val video: String? = null,
+
+    val latitude: Double,
+    val longitude: Double,
+
+    val dokumen: List<String>? = null,
+    val fasilitas: List<String>? = null,
+    val infrastruktur: List<String>? = null,
+
+    val agentImage: String,
+    val agentName: String,
+    val agentPhone: String,
+
+    )
