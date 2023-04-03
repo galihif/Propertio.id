0d2d4c115c6589648fd0c56a9a7acf74c88c32e6
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-03 13:14:04 +0700

    ui: change list property data type

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeViewModel.kt
@@ -1,9 +1,10 @@
 package com.cinurawa.propertioid.ui.pages.home
 
+import Property
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cinurawa.propertioid.data.MainRepository
-import com.cinurawa.propertioid.data.remote.dto.GetAllPropertyDto
 import com.cinurawa.propertioid.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,13 +17,13 @@ class HomeViewModel
     private val repo: MainRepository
 ) : ViewModel() {
 
-    var _listProperty = MutableStateFlow<List<GetAllPropertyDto.PropertyData>>(emptyList())
+    private var _listProperty = MutableStateFlow<List<Property>>(emptyList())
     val listProperty = _listProperty
 
-    var _isLoading = MutableStateFlow(true)
+    private var _isLoading = MutableStateFlow(true)
     val isLoading = _isLoading
 
-    var _error = MutableStateFlow("")
+    private var _error = MutableStateFlow("")
     val error = _error
 
     private fun getListProperty(){
@@ -33,12 +34,14 @@ class HomeViewModel
                         _isLoading.value = true
                     }
                     is Resource.Success -> {
+                        Log.d("GALIH", "getListProperty: ${it.data?.size}")
                         _isLoading.value = false
                         _listProperty.value = if (it.data.isNullOrEmpty()) emptyList() else it.data
                     }
                     is Resource.Error -> {
                         _isLoading.value = false
                         _error.value = it.message ?: "Error"
+                        Log.d("GALIH", "getListProperty: ${it.message}")
                     }
                 }
             }
