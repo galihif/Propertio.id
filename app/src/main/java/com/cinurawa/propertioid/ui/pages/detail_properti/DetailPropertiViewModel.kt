c58f98ba147ebd9cd7b3db5e1ebc41756619f1c4
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-04 16:52:20 +0700

    ui: play youtube video using youtube url extractor

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_properti/DetailPropertiViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_properti/DetailPropertiViewModel.kt
@@ -1,10 +1,17 @@
 package com.cinurawa.propertioid.ui.pages.detail_properti
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.SparseArray
 import androidx.lifecycle.ViewModel
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import at.huber.youtubeExtractor.VideoMeta
+import at.huber.youtubeExtractor.YouTubeExtractor
+import at.huber.youtubeExtractor.YtFile
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+
 
 @HiltViewModel
 class DetailPropertiViewModel
@@ -12,19 +19,43 @@ class DetailPropertiViewModel
     val player: Player
 ) : ViewModel() {
 
-    var _videoUri = ""
+    private var _videoUri = ""
 
     init {
         player.prepare()
     }
 
-    fun addVideoUri(uri: String){
-        _videoUri = uri
-        player.addMediaItem(MediaItem.fromUri(_videoUri))
-    }
+    @SuppressLint("StaticFieldLeak")
+    fun addVideoUri(uri: String, context: Context){
+        object : YouTubeExtractor(context) {
+            @SuppressLint("UnsafeOptInUsageError")
+            override fun onExtractionComplete(
+                ytFiles: SparseArray<YtFile>?,
+                videoMeta: VideoMeta?
+            ) {
+                if (ytFiles != null) {
 
-    fun playVideo(){
-        player.setMediaItem(MediaItem.fromUri(_videoUri))
+                    val iTag = 137//tag of video 1080
+                    // 720, 1080, 480
+                    var videoUrl = ""
+                    val iTags: List<Int> = listOf(22, 137, 18)
+                    for (i in iTags) {
+                        val ytFile = ytFiles.get(i)
+                        if (ytFile != null) {
+                            val downloadUrl = ytFile.url
+                            if (downloadUrl != null && downloadUrl.isNotEmpty()) {
+                                videoUrl = downloadUrl
+                            }
+                        }
+                    }
+                    if (videoUrl == "")
+                        videoUrl = ytFiles[iTag].url
+                    _videoUri = videoUrl
+                    player.setMediaItem(MediaItem.fromUri(videoUrl))
+                }
+            }
+
+        }.extract(uri)
     }
 
     override fun onCleared() {
