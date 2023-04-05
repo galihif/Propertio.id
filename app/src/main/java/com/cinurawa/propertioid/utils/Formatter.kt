8d191fe5e3581d6f7f6f7a653c23472c8280722a
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-05 13:30:03 +0700

    utils: add formatter gmaps uri

--- app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
+++ app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
@@ -1,4 +1,9 @@
 package com.cinurawa.propertioid.utils
 
+import android.net.Uri
+
 fun formatPropertyPhotoUrl(photo: String): String =
     "https://dev.propertio.id/data/image/property/property_photos/$photo"
+
+fun formatGmapsUri(label:String,lat: Double, long: Double, zoom:Int = 15): Uri =
+    Uri.parse("geo:$lat,$long?q=$lat,$long($label)&z=$zoom")
