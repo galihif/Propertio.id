e2a62d9476036f36c07f73c6a31381e641bd7a27
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-05 12:18:40 +0700

    utils: map virtual tour url from api

--- app/src/main/java/com/cinurawa/propertioid/utils/Mapper.kt
+++ app/src/main/java/com/cinurawa/propertioid/utils/Mapper.kt
@@ -37,8 +37,7 @@ fun GetAllPropertyDto.PropertyData.toModel(): Property =
         phoneLine = this.phoneLine,
         isFurniture = this.isFurniture == 1,
 
-//        virtualTour = if (this.propertyVirtualTour.isNotEmpty()) this.propertyVirtualTour[0].file else "",
-        virtualTour = "https://virtualtour0001.propertio.id/",
+        virtualTour = if (this.propertyVirtualTour.isNotEmpty()) this.propertyVirtualTour[0].file else "",
         video = if (this.propertyVideo.isNotEmpty()) this.propertyVideo[0].link else "",
 
         latitude = this.latitude,
