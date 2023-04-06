6b3af4bf3567e63957ad485a613ea4948825fe4b
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-06 11:20:12 +0700

    utils: add decode and encode url string

--- app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
+++ app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
@@ -2,8 +2,16 @@ package com.cinurawa.propertioid.utils
 
 import android.net.Uri
 
-fun formatPropertyPhotoUrl(photo: String): String =
-    "https://dev.propertio.id/data/image/property/property_photos/$photo"
+fun encodeUrl(url: String): String =
+    Uri.encode(url)
+
+fun decodeUrl(url: String): String =
+    Uri.decode(url)
+fun formatPropertyPhotoUrl(id: String): String =
+    "https://dev.propertio.id/data/image/property/property_photos/$id"
+
+fun formatPropertyDocumentUrl(id: String): String =
+    "https://dev.propertio.id/data/file/property/property_documents/$id"
 
 fun formatGmapsUri(label:String,lat: Double, long: Double, zoom:Int = 15): Uri =
     Uri.parse("geo:$lat,$long?q=$lat,$long($label)&z=$zoom")
