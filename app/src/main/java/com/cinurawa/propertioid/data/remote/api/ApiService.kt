e23c731a86c4ce69bbf381ba2cbc50630e1b6104
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-08 10:47:25 +0700

    data: create fun to get all project

--- app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
+++ app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
@@ -1,11 +1,15 @@
 package com.cinurawa.propertioid.data.remote.api
 
+import com.cinurawa.propertioid.data.remote.dto.GetAllProjectDto
 import com.cinurawa.propertioid.data.remote.dto.GetAllPropertyDto
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("mobile/get_all_property_active")
-    suspend fun getAllProperty(
+    suspend fun getAllProperty():GetAllPropertyDto
 
-    ):GetAllPropertyDto
+    @GET("mobile/get_all_project_active")
+    suspend fun getAllProject():GetAllProjectDto
+
+
 }
