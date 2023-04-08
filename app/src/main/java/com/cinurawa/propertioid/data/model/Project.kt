5fb07b8fa59b90b0c540f2d2505f276aae49b999
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-08 10:51:09 +0700

    data: create Project.kt model

---
+++ app/src/main/java/com/cinurawa/propertioid/data/model/Project.kt
@@ -0,0 +1,37 @@
+package com.cinurawa.propertioid.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Project(
+    val id: Int,
+    val name: String,
+    val desc: String,
+    val address: String,
+    val startPrice: Int,
+    val finalPrice: Int,
+    val code: String,
+
+    val photosUrl: List<String>,
+
+    val type: String,
+    val certificate: String,
+
+    val virtualTour: String? = null,
+    val site3DPlan: String? = null,
+    val arApps: String? = null,
+    val video: String? = null,
+
+    val latitude: Double,
+    val longitude: Double,
+
+    val dokumen: List<String>? = null,
+    val fasilitas: List<String>? = null,
+    val infrastruktur: List<Infrastructure>? = null,
+
+    val agentImage: String,
+    val agentName: String,
+    val agentPhone: String,
+
+    ) : Parcelable
