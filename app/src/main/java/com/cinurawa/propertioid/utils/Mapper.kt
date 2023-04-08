14383d5a0db95e8af3b2fb29de581e581c0fb0e4
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-08 10:50:57 +0700

    utils: map project dto to model

--- app/src/main/java/com/cinurawa/propertioid/utils/Mapper.kt
+++ app/src/main/java/com/cinurawa/propertioid/utils/Mapper.kt
@@ -1,7 +1,9 @@
 package com.cinurawa.propertioid.utils
 
 import com.cinurawa.propertioid.data.model.Infrastructure
+import com.cinurawa.propertioid.data.model.Project
 import com.cinurawa.propertioid.data.model.Property
+import com.cinurawa.propertioid.data.remote.dto.GetAllProjectDto
 import com.cinurawa.propertioid.data.remote.dto.GetAllPropertyDto
 
 fun GetAllPropertyDto.PropertyData.toModel(): Property =
@@ -51,3 +53,34 @@ fun GetAllPropertyDto.PropertyData.toModel(): Property =
         agentName = if (this.contactProperty.isNotEmpty()) this.contactProperty[0].name else "",
         agentPhone = if (this.contactProperty.isNotEmpty()) this.contactProperty[0].phone else "",
     )
+
+fun GetAllProjectDto.Data.toModel() : Project =
+    Project(
+        id = this.id,
+        name = this.title,
+        desc = this.description.orEmpty(),
+        address = "${this.address}, ${this.district}, ${this.city} ${this.province} ${this.postalCode}",
+        startPrice = this.priceStart,
+        finalPrice = this.priceFinal,
+        code = this.projectCode,
+
+        photosUrl = this.projectPhoto.map { formatProjectPhotoUrl(it.file) },
+
+        type = this.propertyType.name,
+        certificate = this.certificate,
+
+        virtualTour = if (this.projectVirtualTour.isNotEmpty()) this.projectVirtualTour[0].file else "",
+        video = if (this.projectVideo.isNotEmpty()) this.projectVideo[0].link else "",
+
+        latitude = this.latitude,
+        longitude = this.longitude,
+
+        dokumen = this.projectDocument.map { it.file },
+        fasilitas = this.projectFacility.map { it.facilityType.name },
+        infrastruktur = this.projectInfrastructure.map { Infrastructure(it.name,it.distance) },
+
+        agentImage = "",
+        agentName = if (this.contactProject.isNotEmpty()) this.contactProject[0].name else "",
+        agentPhone = if (this.contactProject.isNotEmpty()) this.contactProject[0].phone else "",
+    )
+
