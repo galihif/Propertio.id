cbc7b16e0f5375df9f028fb33f1306505cae4917
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-08 16:59:02 +0700

    ui: add function to open map

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_project/DetailProjectViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_project/DetailProjectViewModel.kt
@@ -1,8 +1,12 @@
 package com.cinurawa.propertioid.ui.pages.detail_project
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import com.cinurawa.propertioid.utils.formatGmapsUri
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -13,6 +17,23 @@ class DetailProjectViewModel
 ) : ViewModel() {
 
     var _videoUri = ""
+
+    private var _locationName = ""
+    private var _latitude = 0.0
+    private var _longitude = 0.0
+
+    fun addLocation(locationName:String,latitude: Double, longitude: Double){
+        _locationName = locationName
+        _latitude = latitude
+        _longitude = longitude
+    }
+
+    fun openMap(context: Context){
+        val mapUri: Uri = formatGmapsUri(_locationName,_latitude,_longitude)
+        val mapIntent = Intent(Intent.ACTION_VIEW, mapUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+        context.startActivity(mapIntent)
+    }
 
     init {
         player.prepare()
