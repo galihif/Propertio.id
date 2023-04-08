364c3d8628a0421136652d3e0de528b704fc9db4
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-08 12:09:58 +0700

    uiL overloading HargaColumn.kt

--- app/src/main/java/com/cinurawa/propertioid/ui/molecules/HargaColumn.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/molecules/HargaColumn.kt
@@ -5,14 +5,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun HargaColumn(
+    modifier: Modifier = Modifier,
     harga:Int,
     hargaTitle: String = "Harga",
 ) {
     Column(
+        modifier,
         verticalArrangement = Arrangement.spacedBy(4.dp),
     ) {
         Text(
@@ -21,6 +24,28 @@ fun HargaColumn(
         )
         Text(
             text = "Rp. $harga",
+            style = MaterialTheme.typography.h6,
+            color = MaterialTheme.colors.primary
+        )
+    }
+}
+@Composable
+fun HargaColumn(
+    modifier: Modifier = Modifier,
+    hargaTerendah:Int,
+    hargaTertinggi:Int,
+    hargaTitle: String = "Harga",
+) {
+    Column(
+        modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+    ) {
+        Text(
+            text = hargaTitle,
+            style = MaterialTheme.typography.body2
+        )
+        Text(
+            text = "Rp. $hargaTerendah - Rp. $hargaTertinggi",
             style = MaterialTheme.typography.h6,
             color = MaterialTheme.colors.primary
         )
