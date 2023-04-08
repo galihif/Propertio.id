eb75579bbbf94087b16cdbc0d1c8f6a60be7c27c
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-08 10:50:25 +0700

    ui: get list property from repo

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.cinurawa.propertioid.ui.pages.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cinurawa.propertioid.data.MainRepository
+import com.cinurawa.propertioid.data.model.Project
 import com.cinurawa.propertioid.data.model.Property
 import com.cinurawa.propertioid.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,8 +20,14 @@ class HomeViewModel
     private var _listProperty = MutableStateFlow<List<Property>>(emptyList())
     val listProperty = _listProperty
 
+    private var _listProject = MutableStateFlow<List<Project>>(emptyList())
+    val listProject = _listProject
+
     private var _isPropertyLoading = MutableStateFlow(true)
     val isPropertyLoading = _isPropertyLoading
+
+    private var _isProjectLoading = MutableStateFlow(true)
+    val isProjectLoading = _isProjectLoading
 
     private var _error = MutableStateFlow("")
     val error = _error
@@ -46,8 +53,29 @@ class HomeViewModel
         }
     }
 
+    private fun getListProject() {
+        viewModelScope.launch {
+            repo.getAllProject().collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        _isProjectLoading.value = true
+                    }
+                    is Resource.Success -> {
+                        _isProjectLoading.value = false
+                        _listProject.value =
+                            if (it.data.isNullOrEmpty()) emptyList() else it.data.take(3)
+                    }
+                    is Resource.Error -> {
+                        _isProjectLoading.value = false
+                        _error.value = it.message ?: "Error"
+                    }
+                }
+            }
+        }
+    }
 
     init {
         getListProperty()
+        getListProject()
     }
 }
