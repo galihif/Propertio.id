e1cdc244919b8972b07189bbe0d653ffe0507b6c
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-11 11:16:08 +0700

    ui: add playvideo youtube

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_project/DetailProjectViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_project/DetailProjectViewModel.kt
@@ -1,11 +1,13 @@
 package com.cinurawa.propertioid.ui.pages.detail_project
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import com.cinurawa.propertioid.ui.utils.getPlayableYoutubeUrl
 import com.cinurawa.propertioid.utils.formatGmapsUri
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -16,7 +18,7 @@ class DetailProjectViewModel
     val player: Player
 ) : ViewModel() {
 
-    var _videoUri = ""
+    private var _videoUri = ""
 
     private var _locationName = ""
     private var _latitude = 0.0
@@ -39,13 +41,12 @@ class DetailProjectViewModel
         player.prepare()
     }
 
-    fun addVideoUri(uri: String){
-        _videoUri = uri
-        player.addMediaItem(MediaItem.fromUri(_videoUri))
-    }
-
-    fun playVideo(){
-        player.setMediaItem(MediaItem.fromUri(_videoUri))
+    @SuppressLint("StaticFieldLeak")
+    fun addVideoUri(uri: String, context: Context){
+        getPlayableYoutubeUrl(context, uri) {
+            _videoUri = it
+            player.addMediaItem(MediaItem.fromUri(_videoUri))
+        }
     }
 
     override fun onCleared() {
