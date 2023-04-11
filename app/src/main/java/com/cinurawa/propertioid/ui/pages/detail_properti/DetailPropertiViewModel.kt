5a242197a11e6e62c7dd3e9898cfeb2db074ac1e
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-11 10:28:53 +0700

    refactor: using getPlayableYoutubeUrl helper

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_properti/DetailPropertiViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_properti/DetailPropertiViewModel.kt
@@ -4,13 +4,10 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.util.SparseArray
 import androidx.lifecycle.ViewModel
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
-import at.huber.youtubeExtractor.VideoMeta
-import at.huber.youtubeExtractor.YouTubeExtractor
-import at.huber.youtubeExtractor.YtFile
+import com.cinurawa.propertioid.ui.utils.getPlayableYoutubeUrl
 import com.cinurawa.propertioid.utils.formatGmapsUri
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -47,35 +44,10 @@ class DetailPropertiViewModel
 
     @SuppressLint("StaticFieldLeak")
     fun addVideoUri(uri: String, context: Context){
-        object : YouTubeExtractor(context) {
-            @SuppressLint("UnsafeOptInUsageError")
-            override fun onExtractionComplete(
-                ytFiles: SparseArray<YtFile>?,
-                videoMeta: VideoMeta?
-            ) {
-                if (ytFiles != null) {
-
-                    val iTag = 137//tag of video 1080
-                    // 720, 1080, 480
-                    var videoUrl = ""
-                    val iTags: List<Int> = listOf(22, 137, 18)
-                    for (i in iTags) {
-                        val ytFile = ytFiles.get(i)
-                        if (ytFile != null) {
-                            val downloadUrl = ytFile.url
-                            if (downloadUrl != null && downloadUrl.isNotEmpty()) {
-                                videoUrl = downloadUrl
-                            }
-                        }
-                    }
-                    if (videoUrl == "")
-                        videoUrl = ytFiles[iTag].url
-                    _videoUri = videoUrl
-                    player.setMediaItem(MediaItem.fromUri(videoUrl))
-                }
-            }
-
-        }.extract(uri)
+        getPlayableYoutubeUrl(context, uri) {
+            _videoUri = it
+            player.addMediaItem(MediaItem.fromUri(_videoUri))
+        }
     }
 
     override fun onCleared() {
