8a21a8cc8d20e34c4942ef4af0bfa377ca39fd75
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-11 11:34:05 +0700

    ui: create ProjectViewModel.kt

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/project/ProjectViewModel.kt
@@ -0,0 +1,51 @@
+package com.cinurawa.propertioid.ui.pages.project
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cinurawa.propertioid.data.MainRepository
+import com.cinurawa.propertioid.data.model.Project
+import com.cinurawa.propertioid.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProjectViewModel
+@Inject constructor(
+    private val repo: MainRepository
+) : ViewModel() {
+    private var _listProject = MutableStateFlow<List<Project>>(emptyList())
+    val listProject = _listProject
+    private var _isProjectLoading = MutableStateFlow(true)
+    val isProjectLoading = _isProjectLoading
+
+    private var _error = MutableStateFlow("")
+    val error = _error
+
+    private fun getListProject() {
+        viewModelScope.launch {
+            repo.getAllProject().collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        _isProjectLoading.value = true
+                    }
+                    is Resource.Success -> {
+                        _isProjectLoading.value = false
+                        _listProject.value =
+                            if (it.data.isNullOrEmpty()) emptyList() else it.data.take(3)
+                    }
+                    is Resource.Error -> {
+                        _isProjectLoading.value = false
+                        _error.value = it.message ?: "Error"
+                    }
+                }
+            }
+        }
+    }
+
+    init {
+        getListProject()
+    }
+
+}
