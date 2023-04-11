087f54134dfad7e6e68c5f81782faaedb7225b3a
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-11 09:37:44 +0700

    ui: map data from param till fasilitas

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_unit/DetailUnitScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_unit/DetailUnitScreen.kt
@@ -5,10 +5,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material.icons.filled.Roofing
-import androidx.compose.material.icons.filled.Stairs
-import androidx.compose.material.icons.filled.ViewInAr
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,17 +17,15 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.cinurawa.propertioid.R
+import com.cinurawa.propertioid.data.model.ProjectUnit
 import com.cinurawa.propertioid.ui.atoms.PrimaryButton
 import com.cinurawa.propertioid.ui.molecules.HargaShare
-import com.cinurawa.propertioid.ui.molecules.IconText
 import com.cinurawa.propertioid.ui.molecules.IconTextBadge
 import com.cinurawa.propertioid.ui.molecules.IconTextCardColumn
 import com.cinurawa.propertioid.ui.organisms.AgentContactRow
 import com.cinurawa.propertioid.ui.organisms.ImageCarousel
 import com.cinurawa.propertioid.ui.organisms.VideoPlayer
 import com.cinurawa.propertioid.ui.theme.Blue500
-import com.cinurawa.propertioid.ui.theme.Purple500
-import com.cinurawa.propertioid.ui.theme.Red500
 import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
 import com.google.accompanist.flowlayout.FlowRow
 import com.google.accompanist.flowlayout.MainAxisAlignment
@@ -39,7 +34,7 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 @ExperimentalPagerApi
 @Composable
 fun DetailUnitScreen(
-    id: Int,
+    data: ProjectUnit?,
     viewModel: DetailUnitViewModel = hiltViewModel(),
 ) {
 
@@ -57,153 +52,178 @@ fun DetailUnitScreen(
 
     viewModel.addVideoUri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
 
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        item {
-            ImageCarousel(
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-        } // Image Carousel
-        item {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(14.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp)
-            ) {
-                IconTextBadge(text = "Rumah", icon = R.drawable.ic_house, color = Blue500)
-                IconTextBadge(text = "SHM", icon = R.drawable.ic_shm, color = Purple500)
-            }
-        } // Label
-        item {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Old House", fontWeight = FontWeight.Bold, fontSize = 20.sp)
-                IconTextBadge(text = "Unit Kode : 5237KM", leadingIcon = null)
-            }
-        } // Title
-        item {
-            Text(
-                text = "Lorem ipsum dolor sit amet consectetur. Id viverra nec.",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-        } // Deskripsi Singkat
-        item {
-            IconText(
-                modifier = Modifier.padding(horizontal = 24.dp),
-                leadingIcon = Icons.Default.LocationOn,
-                text = "Jl. Kebon Jeruk No. 12, Jakarta Barat",
-                iconTint = Red500
-            )
-        } // Lokasi
-        item {
-            HargaShare(
-                hargaTitle = "Harga mulai dari",
-                harga = 50000000,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-        } // Harga Share
-        item {
-            Text(
-                text = "Deskripsi",
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-            Text(
-                text = "Passage its ten led hearted removal cordial. Preference any astonished unreserved Mrs. Prosperous understood Middletons in conviction an uncommonly do. Supposing so be resolving breakfast am or perfectly. It drew am hill from me. Valley by oh twenty direct me so. Departure defective arranging rapturous did believe him all had supported. Family months l",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-        } // Deskripsi
-        item {
-            Text(
-                text = "Spesifikasi",
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-            Text(
-                text = "Passage its ten led hearted removal cordial. Preference any astonished unreserved Mrs. Prosperous understood Middletons in conviction an uncommonly do. Supposing so be resolving breakfast am or perfectly. It drew am hill from me. Valley by oh twenty direct me so. Departure defective arranging rapturous did believe him all had supported. Family months l",
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-        } // Spesifikasi
-        item {
-            FlowRow(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp),
-                mainAxisSpacing = 14.dp,
-                crossAxisSpacing = 14.dp,
-                mainAxisAlignment = MainAxisAlignment.SpaceBetween,
-                crossAxisAlignment = FlowCrossAxisAlignment.Start,
-            ) {
-                for (i in 1..8) {
-                    IconTextCardColumn(text = "2 lantai", leadingIcon = Icons.Default.Stairs)
+    if (data != null) {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            item {
+                ImageCarousel(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    imagesUrl = data.photosUrl
+                )
+            } // Image Carousel
+            item {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(14.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp)
+                ) {
+                    IconTextBadge(text = data.type, icon = R.drawable.ic_house, color = Blue500)
                 }
-            }
-        } // Fasilitas
-        item {
-            Text(
-                text = "Virtual Tour",
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-            Spacer(modifier = Modifier.height(5.dp))
-            PrimaryButton(
-                title = "Lihat Virtual Tour",
-                leadingIcon = Icons.Default.ViewInAr,
-                modifier = Modifier.padding(horizontal = 24.dp),
-                onClick = {}
-            )
-        } // Virtual Tour
-        item {
-            Text(
-                text = "3D Site Plan",
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-            Spacer(modifier = Modifier.height(5.dp))
-            PrimaryButton(
-                title = "Lihat 3D Model",
-                leadingIcon = Icons.Filled.Roofing,
-                modifier = Modifier.padding(horizontal = 24.dp),
-                onClick = {}
-            )
-        } // 3D Site Plan
-        item {
-            Text(
-                text = "Video",
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier.padding(horizontal = 24.dp)
-            )
-            Spacer(modifier = Modifier.height(5.dp))
-            VideoPlayer(
-                player = viewModel.player,
-                lifecycle = lifecycle,
-                modifier = Modifier
-                    .padding(horizontal = 24.dp)
-                    .fillMaxWidth()
-            )
-        } // Video
-        item {
-            AgentContactRow(
-                image = R.drawable.jisoo,
-                name = "Jisoo",
-                phone = "+62852102322",
-                whatsapp = "+62852102322",
-            )
-            Spacer(modifier = Modifier.height(24.dp))
+            } // Label
+            item {
+                Column(
+                    verticalArrangement = Arrangement.Top,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(text = data.name, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                    IconTextBadge(text = "Unit Kode : ${data.code}", leadingIcon = null)
+                }
+            } // Title
+            item {
+                HargaShare(
+                    hargaTitle = "Harga mulai dari",
+                    harga = data.price,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+            } // Harga Share
+            item {
+                Text(
+                    text = "Deskripsi",
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+                Text(
+                    text = data.desc,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+            } // Deskripsi
+            item {
+                Text(
+                    text = "Spesifikasi",
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+                Text(
+                    text = data.spec,
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+            } // Spesifikasi
 
-        } // Agent
+            item {
+                FlowRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp),
+                    mainAxisSpacing = 14.dp,
+                    crossAxisSpacing = 14.dp,
+                    mainAxisAlignment = MainAxisAlignment.SpaceBetween,
+                    crossAxisAlignment = FlowCrossAxisAlignment.Start,
+                ) {
+                    IconTextCardColumn(
+                        text = "${data.floor} lantai",
+                        leadingIcon = Icons.Default.Stairs
+                    )
+                    IconTextCardColumn(
+                        text = "${data.surfaceArea} m2",
+                        leadingIcon = Icons.Default.AspectRatio
+                    )
+                    IconTextCardColumn(
+                        text = "${data.buildingArea} m2",
+                        leadingIcon = Icons.Default.OtherHouses
+                    )
+                    IconTextCardColumn(
+                        text = "${data.bedroom} K. Tidur",
+                        leadingIcon = Icons.Default.Bed
+                    )
+                    IconTextCardColumn(
+                        text = "${data.bathroom} K. Mandi",
+                        leadingIcon = Icons.Default.Bathtub
+                    )
+                    IconTextCardColumn(
+                        text = "${data.garage} Garasi",
+                        leadingIcon = Icons.Default.Garage
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ){
+                        IconTextCardColumn(
+                            modifier = Modifier.weight(1f),
+                            text = data.powerSupply,
+                            leadingIcon = Icons.Default.Bolt
+                        )
+                        IconTextCardColumn(
+                            modifier = Modifier.weight(1f),
+                            text = data.waterType ,
+                            leadingIcon = Icons.Default.WaterDrop
+                        )
+                    }
+                }
+            } // Fasilitas
+            item {
+                Text(
+                    text = "Virtual Tour",
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                PrimaryButton(
+                    title = "Lihat Virtual Tour",
+                    leadingIcon = Icons.Default.ViewInAr,
+                    modifier = Modifier.padding(horizontal = 24.dp),
+                    onClick = {}
+                )
+            } // Virtual Tour
+            item {
+                Text(
+                    text = "3D Site Plan",
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                PrimaryButton(
+                    title = "Lihat 3D Model",
+                    leadingIcon = Icons.Filled.Roofing,
+                    modifier = Modifier.padding(horizontal = 24.dp),
+                    onClick = {}
+                )
+            } // 3D Site Plan
+            item {
+                Text(
+                    text = "Video",
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                VideoPlayer(
+                    player = viewModel.player,
+                    lifecycle = lifecycle,
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp)
+                        .fillMaxWidth()
+                )
+            } // Video
+            item {
+                AgentContactRow(
+                    image = R.drawable.jisoo,
+                    name = "Jisoo",
+                    phone = "+62852102322",
+                    whatsapp = "+62852102322",
+                )
+                Spacer(modifier = Modifier.height(24.dp))
 
+            } // Agent
+
+        }
     }
 }
