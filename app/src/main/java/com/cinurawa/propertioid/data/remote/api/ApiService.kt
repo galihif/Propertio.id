676c444448c21a0a7e8b328c8611e08b69fccfcf
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-12 12:17:36 +0700

    data: create fun getDetailAgent

--- app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
+++ app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
@@ -3,17 +3,24 @@ package com.cinurawa.propertioid.data.remote.api
 import com.cinurawa.propertioid.data.remote.dto.GetAllAgentDto
 import com.cinurawa.propertioid.data.remote.dto.GetAllProjectDto
 import com.cinurawa.propertioid.data.remote.dto.GetAllPropertyDto
+import com.cinurawa.propertioid.data.remote.dto.GetDetailAgentDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("mobile/get_all_property_active")
-    suspend fun getAllProperty():GetAllPropertyDto
+    suspend fun getAllProperty(): GetAllPropertyDto
 
     @GET("mobile/get_all_project_active")
     suspend fun getAllProject(): GetAllProjectDto
 
     @GET("mobile/get_all_agent")
     suspend fun getAllAgent(): GetAllAgentDto
+
+    @GET("mobile/get_detail_agent/{agentId}")
+    suspend fun getDetailAgent(
+        @Path("agentId") agentId: Int
+    ): GetDetailAgentDto
 
 
 }
