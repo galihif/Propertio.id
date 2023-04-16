08c985ea015ec74add2a174f5455de64a1578fb7
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-16 16:16:04 +0700

    data: create model Developer.kt

---
+++ app/src/main/java/com/cinurawa/propertioid/data/model/Developer.kt
@@ -0,0 +1,12 @@
+package com.cinurawa.propertioid.data.model
+
+data class Developer(
+    var id:Int = 0,
+    var name:String ="",
+    var address:String ="",
+    var imageUrl:String ="",
+    var projectCount:Int = 0,
+){
+    var phone = ""
+    var projectList = ArrayList<Project>()
+}
