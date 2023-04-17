726630286c37a7e30873208efd176aaf9106e5f8
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-17 21:48:58 +0700

    ui: create error state

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/home/HomeScreen.kt
@@ -7,13 +7,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -72,225 +76,121 @@ fun HomeScreen(
             Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
         }
     }
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth(),
-        contentPadding = PaddingValues(24.dp),
-        verticalArrangement = Arrangement.Top
-    ) {
-        item {
-            HomeBanner()
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            HomeTab(selectedMenu) {
-                selectedMenu = it
-            }
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            PropertySearchBox(
-                options = listOptions,
-                onOptionSelected = { selectedOption = it },
-                selectedOption = selectedOption,
-                keyword = keyword,
-                onKeywordChanged = { keyword = it }
-            )
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            HomeCarousel(modifier = Modifier.fillMaxWidth())
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            TitleSectionText(
-                title = "Rekomendasi Properti",
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-        item {
-            if (isPropertyLoading) {
-                Spacer(modifier = Modifier.height(24.dp))
-                LoadingItem()
-                LoadingItem()
-            }
-        }
-        items(listProperty) { property ->
-            Spacer(modifier = Modifier.height(24.dp))
-            PropertyItem(
-                onDetailClicked = { onPropertyClicked(property) },
-                data = property
-            )
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            LihatSemuaButton(
-                Modifier.fillMaxWidth(),
-                onClick = onLihatSemuaPropertyClicked
-            )
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            TitleSectionText(
-                title = "Project Pilihan",
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-        item {
-            if (isProjectLoading) {
-                Spacer(modifier = Modifier.height(24.dp))
-                LoadingItem()
-                LoadingItem()
-            }
-        }
-        items(listProject) {
-            Spacer(modifier = Modifier.height(24.dp))
-            ProjectItem(
-                onDetailClicked = { onProjectClicked(it) },
-                data = it
-            )
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            LihatSemuaButton(
-                Modifier.fillMaxWidth(),
-                onClick = onLihatSemuaProjectClicked
-            )
-        }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-                elevation = 0.dp,
-                backgroundColor = MaterialTheme.colors.primary,
-                shape = RoundedCornerShape(14.dp)
-            ) {
-                Column(
-                    Modifier.padding(14.dp),
-                    verticalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_market_outlook),
-                        contentDescription = ""
-                    )
-                    Text(
-                        "Priopertio.com Indonesia Property Market Outlook 2023",
-                        color = MaterialTheme.colors.onPrimary,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 16.sp
-                    )
-                    Button(
-                        onClick = { /*TODO*/ },
-                        shape = RoundedCornerShape(6.dp),
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = MaterialTheme.colors.surface,
-                            contentColor = MaterialTheme.colors.primary
-                        ),
-                        contentPadding = PaddingValues(10.dp),
-                        elevation = ButtonDefaults.elevation(0.dp)
-                    ) {
-                        Text("Selengkapnya")
-                    }
-                }
-            }
-            Spacer(modifier = Modifier.height(14.dp))
-            Row(horizontalArrangement = Arrangement.spacedBy(14.dp)) {
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-                    elevation = 0.dp,
-                    backgroundColor = DarkBlue500,
-                    shape = RoundedCornerShape(14.dp)
-                ) {
-                    Column(
-                        Modifier.padding(14.dp),
-                        verticalArrangement = Arrangement.spacedBy(12.dp)
-                    ) {
-                        Text(
-                            "Baca cerita inspiratif para pencari hunian",
-                            color = MaterialTheme.colors.onPrimary,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 16.sp
-                        )
-                        Button(
-                            onClick = { /*TODO*/ },
-                            shape = RoundedCornerShape(6.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = MaterialTheme.colors.surface,
-                                contentColor = DarkBlue500
-                            ),
-                            contentPadding = PaddingValues(10.dp),
-                            elevation = ButtonDefaults.elevation(0.dp)
-                        ) {
-                            Text("Cerita Rumah")
-                        }
-                    }
-                }
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f),
-                    elevation = 0.dp,
-                    backgroundColor = Blue700,
-                    shape = RoundedCornerShape(14.dp)
-                ) {
-                    Column(
-                        Modifier.padding(14.dp),
-                        verticalArrangement = Arrangement.spacedBy(12.dp)
-                    ) {
-                        Text(
-                            "Simak sentimen pencari hunian di Indonesia",
-                            color = MaterialTheme.colors.onPrimary,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 16.sp
-                        )
-                        Button(
-                            onClick = { /*TODO*/ },
-                            shape = RoundedCornerShape(6.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = MaterialTheme.colors.surface,
-                                contentColor = Blue700
-                            ),
-                            contentPadding = PaddingValues(10.dp),
-                            elevation = ButtonDefaults.elevation(0.dp)
-                        ) {
-                            Text("CSS H2 2022")
-                        }
-                    }
-                }
 
-            }
+    if (error.isNotEmpty()){
+        Column(modifier = Modifier.fillMaxSize().padding(24.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Icon(imageVector = Icons.Default.Error, contentDescription = "", modifier = Modifier.size(64.dp), tint = Blue700)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = error, textAlign = TextAlign.Center,style = MaterialTheme.typography.h6.copy(color = DarkBlue500, fontWeight = FontWeight.Bold))
         }
-        item {
-            Spacer(modifier = Modifier.height(24.dp))
-            Card(
-                Modifier.wrapContentSize(),
-                shape = RoundedCornerShape(10.dp),
-                backgroundColor = MaterialTheme.colors.primary,
-            ) {
-                Column(
-                    Modifier.fillMaxWidth()
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentPadding = PaddingValues(24.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            item {
+                HomeBanner()
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                HomeTab(selectedMenu) {
+                    selectedMenu = it
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                PropertySearchBox(
+                    options = listOptions,
+                    onOptionSelected = { selectedOption = it },
+                    selectedOption = selectedOption,
+                    keyword = keyword,
+                    onKeywordChanged = { keyword = it }
+                )
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                HomeCarousel(modifier = Modifier.fillMaxWidth())
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                TitleSectionText(
+                    title = "Rekomendasi Properti",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            item {
+                if (isPropertyLoading) {
+                    Spacer(modifier = Modifier.height(24.dp))
+                    LoadingItem()
+                    LoadingItem()
+                }
+            }
+            items(listProperty) { property ->
+                Spacer(modifier = Modifier.height(24.dp))
+                PropertyItem(
+                    onDetailClicked = { onPropertyClicked(property) },
+                    data = property
+                )
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                LihatSemuaButton(
+                    Modifier.fillMaxWidth(),
+                    onClick = onLihatSemuaPropertyClicked
+                )
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                TitleSectionText(
+                    title = "Project Pilihan",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            item {
+                if (isProjectLoading) {
+                    Spacer(modifier = Modifier.height(24.dp))
+                    LoadingItem()
+                    LoadingItem()
+                }
+            }
+            items(listProject) {
+                Spacer(modifier = Modifier.height(24.dp))
+                ProjectItem(
+                    onDetailClicked = { onProjectClicked(it) },
+                    data = it
+                )
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                LihatSemuaButton(
+                    Modifier.fillMaxWidth(),
+                    onClick = onLihatSemuaProjectClicked
+                )
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                Card(
+                    modifier = Modifier.fillMaxWidth(),
+                    elevation = 0.dp,
+                    backgroundColor = MaterialTheme.colors.primary,
+                    shape = RoundedCornerShape(14.dp)
                 ) {
-                    Image(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(150.dp)
-                            .clip(RoundedCornerShape(10.dp, 10.dp, 0.dp, 0.dp)),
-                        painter = painterResource(id = R.drawable.home_banner),
-                        contentDescription = "",
-                        contentScale = ContentScale.Crop
-                    )
                     Column(
-                        Modifier.padding(14.dp)
+                        Modifier.padding(14.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
                     ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_market_outlook),
+                            contentDescription = ""
+                        )
                         Text(
-                            "Mari kami pandu ke rumah Impianmu",
+                            "Priopertio.com Indonesia Property Market Outlook 2023",
                             color = MaterialTheme.colors.onPrimary,
                             fontWeight = FontWeight.Bold,
                             fontSize = 16.sp
                         )
-                        Spacer(modifier = Modifier.height(8.dp))
                         Button(
                             onClick = { /*TODO*/ },
                             shape = RoundedCornerShape(6.dp),
@@ -302,6 +202,119 @@ fun HomeScreen(
                             elevation = ButtonDefaults.elevation(0.dp)
                         ) {
                             Text("Selengkapnya")
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(14.dp))
+                Row(horizontalArrangement = Arrangement.spacedBy(14.dp)) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        elevation = 0.dp,
+                        backgroundColor = DarkBlue500,
+                        shape = RoundedCornerShape(14.dp)
+                    ) {
+                        Column(
+                            Modifier.padding(14.dp),
+                            verticalArrangement = Arrangement.spacedBy(12.dp)
+                        ) {
+                            Text(
+                                "Baca cerita inspiratif para pencari hunian",
+                                color = MaterialTheme.colors.onPrimary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp
+                            )
+                            Button(
+                                onClick = { /*TODO*/ },
+                                shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = MaterialTheme.colors.surface,
+                                    contentColor = DarkBlue500
+                                ),
+                                contentPadding = PaddingValues(10.dp),
+                                elevation = ButtonDefaults.elevation(0.dp)
+                            ) {
+                                Text("Cerita Rumah")
+                            }
+                        }
+                    }
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f),
+                        elevation = 0.dp,
+                        backgroundColor = Blue700,
+                        shape = RoundedCornerShape(14.dp)
+                    ) {
+                        Column(
+                            Modifier.padding(14.dp),
+                            verticalArrangement = Arrangement.spacedBy(12.dp)
+                        ) {
+                            Text(
+                                "Simak sentimen pencari hunian di Indonesia",
+                                color = MaterialTheme.colors.onPrimary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp
+                            )
+                            Button(
+                                onClick = { /*TODO*/ },
+                                shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = MaterialTheme.colors.surface,
+                                    contentColor = Blue700
+                                ),
+                                contentPadding = PaddingValues(10.dp),
+                                elevation = ButtonDefaults.elevation(0.dp)
+                            ) {
+                                Text("CSS H2 2022")
+                            }
+                        }
+                    }
+
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                Card(
+                    Modifier.wrapContentSize(),
+                    shape = RoundedCornerShape(10.dp),
+                    backgroundColor = MaterialTheme.colors.primary,
+                ) {
+                    Column(
+                        Modifier.fillMaxWidth()
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(150.dp)
+                                .clip(RoundedCornerShape(10.dp, 10.dp, 0.dp, 0.dp)),
+                            painter = painterResource(id = R.drawable.home_banner),
+                            contentDescription = "",
+                            contentScale = ContentScale.Crop
+                        )
+                        Column(
+                            Modifier.padding(14.dp)
+                        ) {
+                            Text(
+                                "Mari kami pandu ke rumah Impianmu",
+                                color = MaterialTheme.colors.onPrimary,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 16.sp
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Button(
+                                onClick = { /*TODO*/ },
+                                shape = RoundedCornerShape(6.dp),
+                                colors = ButtonDefaults.buttonColors(
+                                    backgroundColor = MaterialTheme.colors.surface,
+                                    contentColor = MaterialTheme.colors.primary
+                                ),
+                                contentPadding = PaddingValues(10.dp),
+                                elevation = ButtonDefaults.elevation(0.dp)
+                            ) {
+                                Text("Selengkapnya")
+                            }
                         }
                     }
                 }
