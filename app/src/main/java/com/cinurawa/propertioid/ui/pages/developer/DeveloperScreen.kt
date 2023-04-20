d0169a77a6220d0f7ec3169bbebcedae5d0635f6
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-20 14:23:33 +0700

    ui: using error column molecule

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/developer/DeveloperScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/developer/DeveloperScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.cinurawa.propertioid.ui.atoms.TitleSectionText
+import com.cinurawa.propertioid.ui.molecules.ErrorColumn
 import com.cinurawa.propertioid.ui.organisms.DeveloperItem
 import com.cinurawa.propertioid.ui.organisms.LoadingItem
 
@@ -30,49 +31,54 @@ fun DeveloperScreen(
         viewModel.error
     }.collectAsState()
 
-    LaunchedEffect(error){
+    LaunchedEffect(error) {
         if (error.isNotEmpty()) {
             Toast.makeText(context, error, Toast.LENGTH_SHORT).show()
         }
     }
 
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ){
-        item {
-            TitleSectionText(
-                title = "List Developer",
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-        item{
-            if (isLoading) {
-                LoadingItem(
+    if (error.isNotEmpty()) {
+        ErrorColumn(error = error)
+    } else {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            item {
+                TitleSectionText(
+                    title = "List Developer",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            item {
+                if (isLoading) {
+                    LoadingItem(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 24.dp)
+                    )
+                    LoadingItem(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 24.dp)
+                    )
+                }
+            }
+            items(listDeveloper) {
+                Box(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(horizontal = 24.dp)
-                )
-                LoadingItem(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 24.dp)
-                )
+                ) {
+                    DeveloperItem(
+                        onDetailClicked = {
+                            onDetailClicked(it.id)
+                        },
+                        data = it
+                    )
+                }
+                Spacer(modifier = Modifier.height(24.dp))
             }
         }
-        items(listDeveloper) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp)
-            ) {
-                DeveloperItem(
-                    onDetailClicked = {
-                        onDetailClicked(it.id)
-                    },
-                    data = it
-                )
-            }
-            Spacer(modifier = Modifier.height(24.dp))
-        }
+
     }
 }
