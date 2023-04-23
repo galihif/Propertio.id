a1b94f08033d61a21c1360334f80fbdced37d996
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-04-23 10:49:26 +0700

    di: bindsmainrepo

--- app/src/main/java/com/cinurawa/propertioid/di/RepositoryModule.kt
+++ app/src/main/java/com/cinurawa/propertioid/di/RepositoryModule.kt
@@ -1,41 +1,17 @@
 package com.cinurawa.propertioid.di
 
+import com.cinurawa.propertioid.data.MainRepository
 import com.cinurawa.propertioid.data.MainRepositoryImpl
-import com.cinurawa.propertioid.data.remote.api.ApiService
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-@Module
+@Module(includes = [RemoteModule::class])
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
-    @Provides
+abstract class RepositoryModule {
     @Singleton
-    fun provideApiService(): ApiService {
-        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-
-        val client = OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://dev.propertio.id/api/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .client(client)
-            .build()
-
-        return retrofit.create(ApiService::class.java)
-    }
-
-    @Provides
-    @Singleton
-    fun provideMainRepository(apiService: ApiService) = MainRepositoryImpl(apiService)
-
+    @Binds
+    abstract fun bindsMainRepository(mainRepositoryImpl: MainRepositoryImpl): MainRepository
 }
