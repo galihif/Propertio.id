928787e82ef84ceab346ce6c9c4bb2a23ec35cea
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-04 10:50:51 +0700

    utils: create listPropertyType

--- app/src/main/java/com/cinurawa/propertioid/ui/utils/DataProvider.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/utils/DataProvider.kt
@@ -1,20 +1,7 @@
 package com.cinurawa.propertioid.ui.utils
 
+import com.cinurawa.propertioid.utils.enum.PropertyType
+
 object DataProvider {
-    fun typeList(): List<String> {
-        return listOf(
-            "Apartemen",
-            "Gudang",
-            "Kondominium",
-            "Kosan",
-            "Pabrik",
-            "Perkantoran",
-            "Ruang Usaha",
-            "Ruko",
-            "Rumah",
-            "Rumah Kuno",
-            "Tanah",
-            "Villa"
-        )
-    }
+    val listPropertyType = PropertyType.values().map { it.value }
 }
