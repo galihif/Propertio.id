e676e08c432ce2265a9ffae11ecd83f2209f3448
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-04 11:01:02 +0700

    ui: add route and title

--- app/src/main/java/com/cinurawa/propertioid/ui/model/MenuItem.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/model/MenuItem.kt
@@ -4,5 +4,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 data class MenuItem(
     val title: String,
-    val icon: ImageVector
+    val route:String,
+    val icon: ImageVector,
 )
