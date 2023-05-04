2b830418b1b2f45af34a18252d95a1aacf2feffb
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-04 12:36:37 +0700

    data: getAllProperty with query

--- app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
+++ app/src/main/java/com/cinurawa/propertioid/data/remote/api/ApiService.kt
@@ -3,10 +3,18 @@ package com.cinurawa.propertioid.data.remote.api
 import com.cinurawa.propertioid.data.remote.dto.*
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @GET("mobile/get_all_property_active")
     suspend fun getAllProperty(): GetAllPropertyDto
+    @GET("mobile/get_all_property_active")
+    suspend fun getAllProperty(
+        @Query("title") title: String,
+        @Query("property_type_id") proTypeId: Int,
+        @Query("listing_type") listingType: String,
+    ): GetAllPropertyDto
+
 
     @GET("mobile/get_all_project_active")
     suspend fun getAllProject(): GetAllProjectDto
