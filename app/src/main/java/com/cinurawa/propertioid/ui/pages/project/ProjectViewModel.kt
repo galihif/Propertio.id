1328a48183d88e11d5cf7b6420894c676c3cce88
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-05 15:06:31 +0700

    ui: search project from repo

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/project/ProjectViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/project/ProjectViewModel.kt
@@ -1,9 +1,11 @@
 package com.cinurawa.propertioid.ui.pages.project
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.cinurawa.propertioid.data.MainRepository
 import com.cinurawa.propertioid.data.model.Project
+import com.cinurawa.propertioid.ui.utils.DataProvider
 import com.cinurawa.propertioid.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -22,6 +24,39 @@ class ProjectViewModel
 
     private var _error = MutableStateFlow("")
     val error = _error
+
+    val listPropertyType = DataProvider.listPropertyType
+    val keyword = mutableStateOf("")
+    val selectedProType = mutableStateOf("")
+    fun setQuery(keyword: String, selectedProType: String) {
+        if (keyword == "default" && selectedProType == "default"){
+            getListProject()
+            return
+        }
+        this.keyword.value = keyword
+        this.selectedProType.value = selectedProType
+        searchProject()
+    }
+    fun searchProject() {
+        viewModelScope.launch {
+            repo.getAllProject(keyword.value, selectedProType.value).collect {
+                when (it) {
+                    is Resource.Loading -> {
+                        _isProjectLoading.value = true
+                    }
+                    is Resource.Success -> {
+                        _isProjectLoading.value = false
+                        _listProject.value =
+                            if (it.data.isNullOrEmpty()) emptyList() else it.data.take(3)
+                    }
+                    is Resource.Error -> {
+                        _isProjectLoading.value = false
+                        _error.value = it.message ?: "Error"
+                    }
+                }
+            }
+        }
+    }
 
     private fun getListProject() {
         viewModelScope.launch {
@@ -44,8 +79,7 @@ class ProjectViewModel
         }
     }
 
-    init {
-        getListProject()
-    }
+
+
 
 }
