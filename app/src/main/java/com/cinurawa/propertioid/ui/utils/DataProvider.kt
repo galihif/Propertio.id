02627a3f3f4a54c43c1de07966e923a093f86f05
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-05 14:08:44 +0700

    utils: add listListingType

--- app/src/main/java/com/cinurawa/propertioid/ui/utils/DataProvider.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/utils/DataProvider.kt
@@ -1,7 +1,9 @@
 package com.cinurawa.propertioid.ui.utils
 
+import com.cinurawa.propertioid.utils.enum.ListingType
 import com.cinurawa.propertioid.utils.enum.PropertyType
 
 object DataProvider {
     val listPropertyType = PropertyType.values().map { it.value }
+    val listListingType = ListingType.values().map { it.value }
 }
