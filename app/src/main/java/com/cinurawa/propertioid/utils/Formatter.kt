ac73ccf9485350d44d8b2d2cd078368b1c66bf3c
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-24 14:11:22 +0700

    utils: create getYoutubeWatchUrl

--- app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
+++ app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
@@ -10,14 +10,39 @@ fun formatHarga(value: Long): String {
 fun formatAgentPhotoUrl(id: String): String =
     "https://dev.propertio.id/data/image/users/$id"
 
+fun getYoutubeWatchUrl(url: String): String {
+    return when {
+        url.contains("youtu.be/") -> {
+            val videoId = url.substringAfterLast("/")
+            "https://www.youtube.com/watch?v=$videoId"
+        }
+
+        url.contains("watch?v=") -> {
+            val videoId = url.substringAfterLast("v=")
+            "https://www.youtube.com/watch?v=$videoId"
+        }
+
+        url.contains("/embed/") -> {
+            val videoId = url.substringAfterLast("/embed/")
+            "https://www.youtube.com/watch?v=$videoId"
+        }
+
+        url.length == 11 -> {
+            "https://www.youtube.com/watch?v=$url"
+        }
+
+        else -> {
+            throw IllegalArgumentException("Invalid YouTube URL")
+        }
+    }
+}
+
+
 fun encodeUrl(url: String): String =
     Uri.encode(url)
 
 fun decodeUrl(url: String): String =
     Uri.decode(url)
 
-fun formatPropertyDocumentUrl(id: String): String =
-    "https://drive.google.com/viewerng/viewer?embedded=true&url=https://dev.propertio.id/data/file/property/property_documents/$id"
-
-fun formatGmapsUri(label:String,lat: Double, long: Double, zoom:Int = 15): Uri =
+fun formatGmapsUri(label: String, lat: Double, long: Double, zoom: Int = 15): Uri =
     Uri.parse("geo:$lat,$long?q=$lat,$long($label)&z=$zoom")
