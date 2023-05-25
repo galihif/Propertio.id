cbf56a3ac9f68dbfad73e5bd589e4d84a91a7492
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-25 13:22:40 +0700

    utils: delete function that only used in UI

--- app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
+++ app/src/main/java/com/cinurawa/propertioid/utils/Formatter.kt
@@ -1,11 +1,5 @@
 package com.cinurawa.propertioid.utils
 
-import android.net.Uri
-
-fun formatHarga(value: Long): String {
-    return String.format("%,d", value).replace(",", ".")
-}
-
 
 fun formatAgentPhotoUrl(id: String): String =
     "https://dev.propertio.id/data/image/users/$id"
@@ -36,13 +30,3 @@ fun getYoutubeWatchUrl(url: String): String {
         }
     }
 }
-
-
-fun encodeUrl(url: String): String =
-    Uri.encode(url)
-
-fun decodeUrl(url: String): String =
-    Uri.decode(url)
-
-fun formatGmapsUri(label: String, lat: Double, long: Double, zoom: Int = 15): Uri =
-    Uri.parse("geo:$lat,$long?q=$lat,$long($label)&z=$zoom")
