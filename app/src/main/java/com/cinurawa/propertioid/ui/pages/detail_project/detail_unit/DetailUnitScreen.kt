284e54037338297f8782a094d174618d20173456
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-05-25 14:20:12 +0700

    ui: use DetailProjectViewModel instead of detail unit

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_project/detail_unit/DetailUnitScreen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_project/detail_unit/DetailUnitScreen.kt
@@ -1,19 +1,39 @@
-package com.cinurawa.propertioid.ui.pages.detail_unit
+package com.cinurawa.propertioid.ui.pages.detail_project.detail_unit
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.AspectRatio
+import androidx.compose.material.icons.filled.Bathtub
+import androidx.compose.material.icons.filled.Bed
+import androidx.compose.material.icons.filled.Bolt
+import androidx.compose.material.icons.filled.Garage
+import androidx.compose.material.icons.filled.OtherHouses
+import androidx.compose.material.icons.filled.Roofing
+import androidx.compose.material.icons.filled.Stairs
+import androidx.compose.material.icons.filled.ViewInAr
+import androidx.compose.material.icons.filled.WaterDrop
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import com.cinurawa.propertioid.R
@@ -24,6 +44,7 @@ import com.cinurawa.propertioid.ui.molecules.IconTextBadge
 import com.cinurawa.propertioid.ui.molecules.IconTextCardColumn
 import com.cinurawa.propertioid.ui.organisms.ImageCarousel
 import com.cinurawa.propertioid.ui.organisms.VideoPlayer
+import com.cinurawa.propertioid.ui.pages.detail_project.DetailProjectViewModel
 import com.cinurawa.propertioid.ui.theme.Blue500
 import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
 import com.google.accompanist.flowlayout.FlowRow
@@ -35,8 +56,13 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 fun DetailUnitScreen(
     data: ProjectUnit?,
     onVirtualTourClicked: (String) -> Unit = {},
-    viewModel: DetailUnitViewModel = hiltViewModel(),
+    viewModel: DetailProjectViewModel,
 ) {
+
+    val context = LocalContext.current
+    if (data?.video != null) {
+        viewModel.addVideoUri(data.video,context)
+    }
 
     var lifecycle by remember {
         mutableStateOf(Lifecycle.Event.ON_CREATE)
@@ -49,8 +75,6 @@ fun DetailUnitScreen(
             lifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
-
-    viewModel.addVideoUri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
 
     if (data != null) {
         LazyColumn(
