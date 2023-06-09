8cf1dbf0c0a64e5327aa36ee3cb7314c85efe426
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-06-09 16:27:53 +0700

    change dummy property type

--- app/src/main/java/com/cinurawa/propertioid/utils/DummyData.kt
+++ app/src/main/java/com/cinurawa/propertioid/utils/DummyData.kt
@@ -24,7 +24,7 @@ object DummyData {
                     "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
                     "https://images.unsplash.com/photo-1600047509807-ba8f99d2cdde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=692&q=80"
                 ),
-                type = "House",
+                type = "Rumah",
                 listingType = "Sale",
                 certificate = "SHM",
                 floor = 2,
