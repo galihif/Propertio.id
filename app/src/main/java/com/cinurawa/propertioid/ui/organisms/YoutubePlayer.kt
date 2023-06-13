6420d3bb9ecc1ce6d298cd7ab39985969cee4ed5
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-06-13 14:43:11 +0700

    ui: create YoutubePlayer

---
+++ app/src/main/java/com/cinurawa/propertioid/ui/organisms/YoutubePlayer.kt
@@ -0,0 +1,36 @@
+package com.cinurawa.propertioid.ui.organisms
+
+import android.content.Context
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+@Composable
+fun YoutubePlayer(
+    videoId: String,
+    modifier: Modifier = Modifier,
+    context: Context
+) {
+    AndroidView(
+        factory = {
+            var view = YouTubePlayerView(context)
+            val fragment = view.addYouTubePlayerListener(
+                object : AbstractYouTubePlayerListener() {
+                    override fun onReady(youTubePlayer: YouTubePlayer) {
+                        super.onReady(youTubePlayer)
+                        youTubePlayer.loadVideo(videoId, 0f)
+                    }
+                }
+            )
+            view
+        },
+        modifier = modifier.clip(RoundedCornerShape(8.dp))
+    )
+}
