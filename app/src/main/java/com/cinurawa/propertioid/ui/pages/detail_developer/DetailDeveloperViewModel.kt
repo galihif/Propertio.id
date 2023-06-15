87ba31e1236c86d061fe1b73e14622d5e61daea1
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-06-15 16:25:29 +0700

    move id

--- app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_developer/DetailDeveloperViewModel.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/pages/detail_developer/DetailDeveloperViewModel.kt
@@ -15,39 +15,39 @@ class DetailDeveloperViewModel
 @Inject constructor(
     private val repo: MainRepository
 ) : ViewModel() {
-    private var _id = 0
 
     private var _developer = MutableStateFlow(Developer())
     val developer = _developer
-
     private var _loading = MutableStateFlow(false)
     val loading = _loading
-
     private var _error = MutableStateFlow("")
     val error = _error
 
-    fun setId(id:Int){
+    private var _id = 0
+    fun setId(id: Int) {
         _id = id
         getDetailAgent()
     }
 
-    private fun getDetailAgent(){
+    private fun getDetailAgent() {
         viewModelScope.launch {
             repo.getDetailDeveloper(_id)
-                .collect{
-                    when(it){
+                .collect {
+                    when (it) {
                         is Resource.Loading -> {
                             _loading.value = true
                         }
+
                         is Resource.Success -> {
                             _loading.value = false
-                            if (it.data != null){
+                            if (it.data != null) {
                                 _developer.value = it.data
                             }
                         }
+
                         is Resource.Error -> {
                             _loading.value = false
-                            _error.value = it.message?:"Error"
+                            _error.value = it.message ?: "Error"
                         }
                     }
                 }
