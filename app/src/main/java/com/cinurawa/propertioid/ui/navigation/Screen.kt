bb8a060b426bd5f98f678c37ede07bb593cacf61
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-07-16 12:01:43 +0700

    lint: fix

--- app/src/main/java/com/cinurawa/propertioid/ui/navigation/Screen.kt
+++ app/src/main/java/com/cinurawa/propertioid/ui/navigation/Screen.kt
@@ -1,20 +1,15 @@
 package com.cinurawa.propertioid.ui.navigation
 
-import com.cinurawa.propertioid.utils.enum.PropertyType
-
-
 sealed class Screen(val route: String, val title: String? = null) {
     object Home : Screen("Home", "Home")
-    object Properti : Screen("Properti/{keyword}/{propertyType}/{listingType}", "Properti"){
+    object Properti : Screen("Properti/{keyword}/{propertyType}/{listingType}", "Properti") {
         fun createRoute(
             keyword: String,
             propertyType: String,
             listingType: String
         ) = "Properti/$keyword/$propertyType/$listingType"
     }
-
-
-    object Project : Screen("Project/{keyword}/{propertyType}", "Project"){
+    object Project : Screen("Project/{keyword}/{propertyType}", "Project") {
         fun createRoute(
             keyword: String,
             propertyType: String
@@ -29,19 +24,15 @@ sealed class Screen(val route: String, val title: String? = null) {
     object DetailUnit : Screen("DetailUnit/{id}") {
         fun createRoute(id: Int) = "DetailUnit/$id"
     }
-
     object DetailAgent : Screen("DetailAgent/{id}") {
         fun createRoute(id: Int) = "DetailAgent/$id"
     }
-
     object DetailDeveloper : Screen("DetailDeveloper/{id}") {
         fun createRoute(id: Int) = "DetailDeveloper/$id"
     }
-
     object Webview : Screen("Webview/{url}") {
         fun createRoute(url: String) = "Webview/$url"
     }
-
     object Agent : Screen("Agent")
     object Developer : Screen("Developer")
 }
